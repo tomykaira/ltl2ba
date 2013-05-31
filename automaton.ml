a59e7b71bb9578dbf6f479899e9f9d50c055090e
tomykaira <tomykaira@gmail.com>
2013-05-31 23:17:46 +0900

    Convert automaton to graph

--- automaton.ml
+++ automaton.ml
@@ -1,10 +1,41 @@
-type node = FormulaSet.t
 type link = Epsilon of Ltl.ltl option
             | Sigma of Ltl.ltl option * Ltl.ltl option
-type edge = {
+type node = {
+  name: Ltl.FormulaSet.t;
+  edges: edge list;
+}
+and edge = {
   link : link;
-  s: node; t: node;
+  target: node;
 }
 
+let leaf_node set =
+  { name = set; edges = [] }
+
 let reduction_graph root_set =
-  let g = ref
+  let edges = match Ltl.epsilon_transform root_set with
+    | None -> []
+    | Some(conv_list) ->
+      List.map (fun (set, cond) -> { link = Epsilon(cond); target = leaf_node set }) conv_list
+  in
+  { name = root_set; edges = edges }
+
+let link_to_string = function
+  | Epsilon(None) -> "ε"
+  | Epsilon(Some(formula)) -> "ε, !" ^ (Ltl.to_string(formula))
+  | Sigma(None, None) -> "Σ"
+  | Sigma(Some(cond), None) -> "Σ" ^ (Ltl.to_string(cond))
+  | Sigma(None, Some(post)) -> "Σ, !" ^ (Ltl.to_string(post))
+  | Sigma(Some(cond), Some(post)) -> "Σ" ^ (Ltl.to_string(cond)) ^ ", !" ^ (Ltl.to_string(post))
+
+let to_graph automaton =
+  let set_to_s = Ltl.FormulaSet.to_string in
+  let g = ref (Graph.new_graph "Automaton") in
+  let s_name = (set_to_s automaton.name) in
+  g := Graph.add_start !g s_name;
+  List.iter (fun {link = link; target = target} ->
+    let t_name = (set_to_s target.name) in
+    g := Graph.add_node !g t_name;
+    g := Graph.link !g s_name t_name (link_to_string link)
+  ) automaton.edges;
+  !g
--- main.ml
+++ main.ml
@@ -1,11 +1,5 @@
-open Ltl
-open Graph
-
 let graph_test =
-  let g = new_graph "F_p" in
-  let g = add_start g "1" in
-  let g = add_final g "2" in
-  let g = link g "1" "1" "&Sigma;¬p" in
-  let g = link g "2" "2" "&Sigma;" in
-  let g = link g "1" "2" "&Sigma;p" in
-  print_graph g
+  let formula = Parser.main Lexer.token (Lexing.from_string "G((not p) or (F q))") in
+  let a = Automaton.reduction_graph (Ltl.FormulaSet.singleton formula) in
+  let g = Automaton.to_graph a in
+  Graph.print_graph g
