83d4bf1780abd3ca6784a36cfe90a2bc6a1846fb
tomykaira <tomykaira@gmail.com>
2013-06-01 14:01:44 +0900

    Rewrite main for automaton sometimes

--- main.ml
+++ main.ml
@@ -1,6 +1,17 @@
-let graph_test =
-  let formula = Parser.main Lexer.token (Lexing.from_string "G((not p) or (F q))\n") in
+let file num =
+  open_out_gen [Open_wronly; Open_creat; Open_trunc; Open_text] 0o666 (Printf.sprintf "out_%02d.gv" num)
+
+let rec main num =
+  Printf.printf "%02d> " num;
+  flush stdout;
+  let formula = Ltl.negative_normal_form (Parser.main Lexer.token (Lexing.from_channel stdin)) in
+  print_endline (Ltl.to_string formula);
   let formula_set = (Ltl.FormulaSet.singleton formula) in
   let a = Automaton.construct_gba_from formula_set in
   let g = Automaton.to_graph a in
-  Graph.print_graph stdout g
+  let out = (file num) in
+  Graph.print_graph out g;
+  close_out out;
+  main (num + 1)
+
+let _ = main 0
